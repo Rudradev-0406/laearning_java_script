55f43bf99f5d8c4d1e723e9409c4885b92bea081
RUDRADEV-DEWANGAN-06 <rudradev.dewangan2023@ssipmt.com>
2025-01-28 14:03:08 +0000

    data_types

---
+++ 02_dataTypes/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert("invalid username or id");  we ar using node js not browser
+
+
+let name = "John";
+let age = 25;
+let isloggedin = true;
+
+// premitive data types
+
+// number 
+// BigInt
+// string
+// boolean = true/false
+// null = standlone valur 
+// undefined = variable declared but not assigned its value 
+// symbol = unique value
+
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
