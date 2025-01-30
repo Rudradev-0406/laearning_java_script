ac4bb2c3d32b17af8040aff15bcb5dc7a2e6a911
RUDRADEV-DEWANGAN-06 <rudradev.dewangan2023@ssipmt.com>
2025-01-30 13:51:47 +0000

    container config done

---
+++ 01_basics/comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2<89);
+// console.log(2!=6);
+
+// console.log("2">1);
+// console.log("6"!=6);
+// console.log(null>=1);
+
+// console.log(undefined==0);
+
+ 
+// === checks strictely along with the data types
+
+console.log("2"===2);
+// which is false because the value is same but the data type of both is not the same
---
+++ 01_basics/operator.js
@@ -0,0 +1,25 @@
+// let score =33
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// true=1
+// false=0
+// let isloggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+
+// console.log(booleanIsLoggedIn);
+// 1=> true;
+// 0=> false;
+// "rudra"=>true
+
+
+let randnum = 50
+
+
+let stringnum = String(randnum)
+
+
+console.log(typeof stringnum);
+
+// in this example we have learned that how we can convert any number into strings 
