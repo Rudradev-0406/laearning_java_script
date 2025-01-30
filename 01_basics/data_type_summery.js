12f05b061ad3f73b3462e6764b691eab78248d37
RUDRADEV-DEWANGAN-06 <rudradev.dewangan2023@ssipmt.com>
2025-01-30 14:21:03 +0000

    datatype summery

---
+++ 01_basics/data_type_summery.js
@@ -0,0 +1,31 @@
+// premitive
+
+// 7 types String,Number,Boolean,null,symbol,bigInt
+
+// const score = 200;
+
+// const IsLoggedin =false
+
+// const outsideTemp=null
+
+// const id=Symbol('123')
+
+// const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+
+// reference type(non premitive)
+
+
+// array,objects,functions
+
+// following coede is not working properly
+// const heros = ["rudra", "akshat","gautam","chota don"];
+// {
+//     name:rudradev,
+// }
+
+
+console.log(typeof id);
+console.log(typeof anotherId);
