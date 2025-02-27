79962384184ea1e5d694200bbabec05158ad39fe
RUDRADEV-DEWANGAN-06 <rudradev.dewangan2023@ssipmt.com>
2025-02-27 22:08:57 +0530

    Add files via upload

---
+++ 01_basics/number_and_maths.js
@@ -0,0 +1,44 @@
+// LEARNING ABOUT THE NUMBERS
+
+// const score =400
+
+// const balance =new Number(100)
+// console.log(balance);
+
+// console.log(score);
+
+// console.log(balance.toFixed(2));
+
+// const otherenumber = 100.8283
+
+// console.log(otherenumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+////////////////////*************************************************************/////////////////////////////////////////////////// 
+
+// LAEARNIGN ABOUT THE MATHS
+
+console.log(Math);
+
+console.log(Math.abs(-4)); //absolute value mean the all values will come in the positive tearms0
+
+console.log(Math.round(4.7)); // round off of the number to the nearest integer
+
+console.log(Math.ceil(4.1)); // round off of the number to the nearest integer
+
+console.log(Math.floor(4.9)); // round off of the number to the nearest integer
+
+console.log(Math.sqrt(16)); // square root of the number
+
+console.log(Math.min(1,2,3,4,5,6,7,8,9,10)); // minimum of the numbers
+
+console.log(Math.random()); // random number between 0 and 1
+console.log((Math.random()*10)+1); 
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min) //very very important line of the math function
+
